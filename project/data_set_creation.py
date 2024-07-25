7256eb2bdac5568c7e65cad084de4d2ed41874e9
kai <thebiggestkai@gmail.com>
2024-07-25 14:31:15 -0700

    Add save_data function

--- project/data_set_creation.py
+++ project/data_set_creation.py
@@ -1,26 +1,21 @@
 # Determine number of datasets
+import json
 
-def create_dataset_format(prompt, output):
+
+def format_data(model_response):
     # prep data into key value pairs
     instruction = "You are a technology analyst that analyzes text and summarizes it into easy to read language"
-    user_input = prompt
-    model_output = output
+    response = json.loads(model_response)
     data = {
-        "instruction": instruction,
-        "input": user_input,
-        "output": model_output
+        "instruction": instruction
     }
+    data.update(response)
     return data
 
 
-def make_data():
-    # access model
-    # based off paragraph length ask it to generate number of input and outputs to summarize text
-    # format using create_dataset_format
-    # store each into a growing dictionary
-    pass
-
-
-def save_data():
+def save_data(dataset):
     # store full dictionary into json file
-    pass
+    file_path = "../data/dataset.json"
+
+    with open(file_path, "w") as file:
+        json.dump(dataset, file, indent=4)
