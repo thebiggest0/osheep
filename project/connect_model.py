7fdd3476e5935975193e53bf262ccdeac2b527e7
kai <thebiggestkai@gmail.com>
2024-07-25 09:00:55 -0700

    Implement pseudocode for connect_model.py

---
+++ project/connect_model.py
@@ -0,0 +1,10 @@
+# request response from model
+
+def request_model():
+    # send paragraph and prompt to model to interpret
+    pass
+
+
+def respond_model():
+    # receive response from model
+    pass
