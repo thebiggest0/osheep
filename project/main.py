40895fd8b6e7ec28a37d8a82224b097b11a26aad
Kai <thebiggestkai@gmail.com>
2024-07-26 11:00:12 -0700

    Update functions to work with gemma 2 and mistral

--- project/main.py
+++ project/main.py
@@ -5,18 +5,41 @@ import process_text
 
 def main():
     paragraphs = process_text.break_down_text()
-    dataset = []
-    for paragraph in paragraphs:
-        count = paragraph[1] // 200
+    # dataset = []
+    # for paragraph in paragraphs:
+    count = paragraphs[1] // 120
+    if count == 0:
+        count = 1
 
-        model = "mistral"
-        prompt = f'#based on the content, summarize the paragraph and return {count} dictionary of key value pairs. ' \
-                 f'Example: {{"input": "topic of the paragraph", "response": "summary of the topic"}} ## content: {paragraph[0]}'
+    model_mistral = "mistral"
+    prompt_mistral = f'#based on the content, summarize the paragraph and return only a python list which contains 5 different dictionaries of key value pairs covering key points of the paragraph. Example: {{"instruction": "natural language text input about a topic in this paragraph about AIDA", "response": "target output for the given prompt, per the instructions and context provided"}} ## content: "{paragraphs[0]}"'
+    file_path_mistral = "../data/dataset.json"
+    save_path_mistral = "../data/dataset.json"
+    mistral = False
 
+    model = "gemma2"
+    prompt = f'#based on the ##content, summarize the paragraph and return only a python list (nothing extra) which contains 5 different dictionaries of key value pairs covering key points of the paragraph see ##Example for dictionary key value pair template. ##Example: {{"instruction": "natural language text input about a topic in this paragraph about AIDA", "response": "target output for the given prompt, per the instructions and context provided"}} ## content: "{paragraphs[0]}"'
+    file_path = "../data/dataset_gemma.json"
+    save_path = "../data/dataset_gemma.json"
+    # gemma = False
+
+    if mistral:
+        response = connect_model.request_model(model_mistral, prompt_mistral)
+        print(response)
+
+        cleaned_dataset = data_set_creation.format_data(response)
+        print(cleaned_dataset)
+
+        data_set_creation.save_data(data_set_creation.format_data(response), file_path_mistral, save_path_mistral)
+
+    else:
         response = connect_model.request_model(model, prompt)
-        dataset.append(data_set_creation.format_data(response))
+        print(response)
 
-    data_set_creation.save_data(dataset)
+        cleaned_dataset = data_set_creation.format_data(response)
+        print(cleaned_dataset)
+
+        data_set_creation.save_data(data_set_creation.format_data(response), file_path, save_path)
 
 
 if __name__ == '__main__':
