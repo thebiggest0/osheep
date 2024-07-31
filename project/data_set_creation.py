04ae918e51809f65824a3a8e07ff83750e6995d7
Kai <thebiggestkai@gmail.com>
2024-07-31 14:58:14 -0700

    Add function to generate input for each dataset

--- project/data_set_creation.py
+++ project/data_set_creation.py
@@ -3,6 +3,7 @@ Determine number of datasets
 """
 
 import json
+import connect_model
 
 
 def format_data(model_response):
@@ -27,3 +28,23 @@ def save_data(dataset, file_path, save_path):
 
     with open(file_path, "w") as file:
         json.dump(data, file, indent=4)
+
+
+def confirm_data_format(model, dataset):
+    # # model_prompt = f"## Check if the text under ### Content is a python list of dictionaries with keys: instruction, response \n ### Content: '{dataset} \n ## if there is other text outside of the python list, remove it and return only the list of dictionaries"
+    # model_prompt = f"Validate if the 'Input string' contains a Python list of dictionaries. Only respond with the list containing the dictionary, do not add any other text or description. If there is any text outside the list, remove it and return only the list containing dictionaries as a string. Input string: {dataset}"
+    # return connect_model.request_model(model, model_prompt)
+
+    while dataset[0] != "[":
+        dataset = dataset[1:]
+    while dataset[-1] != "]":
+        dataset = dataset[:-1]
+
+
+def create_input(model, dataset, content):
+    dataset = json.loads(dataset)
+    for data in dataset:
+        model_prompt = f"#Based on the text in the ##Content and the instruction: {data['instruction']} and the response {data['response']}, generate the corresponding input. ##Content: {content}"
+        generated_input = connect_model.request_model(model, model_prompt)
+        data["input"] = generated_input.strip()
+    return dataset
