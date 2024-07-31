4961a7f140a894673660a57bd5b5c567e80b17da
Kai <thebiggestkai@gmail.com>
2024-07-31 14:57:29 -0700

    Add function to break text into mini paragraphs

--- project/process_text.py
+++ project/process_text.py
@@ -12,3 +12,13 @@ def break_down_text():
     list_paragraphs = [new_text, len(new_text)]
 
     return list_paragraphs
+
+
+def text_to_list():
+    file_path = "../data/text_to_summarize.txt"
+    with open(file_path, "r") as text:
+        new_text = text.read()
+
+    list_paragraphs = new_text.split("***")
+    list_paragraphs = [paragraph.strip() for paragraph in list_paragraphs]
+    return list_paragraphs
