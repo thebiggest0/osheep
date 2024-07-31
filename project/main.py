e8fc00818f1e28086c9f196dc93af824504acfcb
Kai <thebiggestkai@gmail.com>
2024-07-31 14:56:54 -0700

    Update functions to handle more text

--- project/main.py
+++ project/main.py
@@ -4,42 +4,108 @@ import process_text
 
 
 def main():
-    paragraphs = process_text.break_down_text()
-    # dataset = []
-    # for paragraph in paragraphs:
-    count = paragraphs[1] // 120
-    if count == 0:
-        count = 1
-
-    model_mistral = "mistral"
-    prompt_mistral = f'#based on the content, summarize the paragraph and return only a python list which contains 5 different dictionaries of key value pairs covering key points of the paragraph. Example: {{"instruction": "natural language text input about a topic in this paragraph about AIDA", "response": "target output for the given prompt, per the instructions and context provided"}} ## content: "{paragraphs[0]}"'
-    file_path_mistral = "../data/dataset.json"
-    save_path_mistral = "../data/dataset.json"
     mistral = False
+    model_mistral = "mistral"
+    file_path_mistral = "../data/aida_dataset_mistral.json"
+    save_path_mistral = "../data/aida_dataset_mistral.json"
 
-    model = "gemma2"
-    prompt = f'#based on the ##content, summarize the paragraph and return only a python list (nothing extra) which contains 5 different dictionaries of key value pairs covering key points of the paragraph see ##Example for dictionary key value pair template. ##Example: {{"instruction": "natural language text input about a topic in this paragraph about AIDA", "response": "target output for the given prompt, per the instructions and context provided"}} ## content: "{paragraphs[0]}"'
-    file_path = "../data/dataset_gemma.json"
-    save_path = "../data/dataset_gemma.json"
-    # gemma = False
+    model_gemma = "gemma2"
+    file_path_gemma = "../data/aida_dataset_gemma.json"
+    save_path_gemma = "../data/aida_dataset_gemma.json"
 
+    paragraphs = process_text.text_to_list()
     if mistral:
-        response = connect_model.request_model(model_mistral, prompt_mistral)
-        print(response)
+        for paragraph in paragraphs:
+            print(paragraph)
+            count = len(paragraph) // 100
+            if count == 0:
+                count = 1
 
-        cleaned_dataset = data_set_creation.format_data(response)
-        print(cleaned_dataset)
+            prompt_mistral = f'#based on the content, summarize the paragraph and return only a python list (nothing extra) which contains {count} different dictionaries of key ("instruction", "response") value pairs covering key points of the paragraph. Example: {{"instruction": "natural language text input about a topic in this paragraph about AIDA", "response": "sentence explanation for the given prompt, per the instructions and context provided"}} ## content: "{paragraph}"'
 
-        data_set_creation.save_data(data_set_creation.format_data(response), file_path_mistral, save_path_mistral)
+            response = connect_model.request_model(model_mistral, prompt_mistral)
+            print(1, response)
+
+            ## TESTING ##
+            # confirm response format is correct with AI
+            data_set_creation.confirm_data_format(model_mistral, response)
+            print(2, response)
+
+            # use AI to provide input to each dictionary
+            response = data_set_creation.create_input(model_mistral, response, paragraphs[0])
+            print(3, response, "\n")
+
+            data_set_creation.save_data(response, file_path_mistral, save_path_mistral)
 
     else:
-        response = connect_model.request_model(model, prompt)
-        print(response)
+        for paragraph in paragraphs:
+            print(paragraph)
+            count = len(paragraph) // 100
+            if count == 0:
+                count = 1
 
-        cleaned_dataset = data_set_creation.format_data(response)
-        print(cleaned_dataset)
+            model_mistral = "mistral"
+            prompt_mistral = f'#based on the content, summarize the paragraph and return only a python list (nothing extra) which contains {count} different dictionaries each with keys ("instruction", "response") value pairs covering key points of the paragraph. Example: {{"instruction": "natural language text input about a topic in this paragraph about AIDA", "response": "sentence explanation for the given prompt, per the instructions and context provided"}} ## content: "{paragraph}"'
+            file_path_mistral = "../data/aida_dataset_mistral.json"
+            save_path_mistral = "../data/aida_dataset_mistral.json"
 
-        data_set_creation.save_data(data_set_creation.format_data(response), file_path, save_path)
+            response = connect_model.request_model(model_gemma, prompt_mistral)
+            print(1, response)
+
+            ## TESTING ##
+            # confirm response format is correct with AI
+            data_set_creation.confirm_data_format(model_gemma, response)
+            print(2, response)
+
+            # use AI to provide input to each dictionary
+            response = data_set_creation.create_input(model_gemma, response, paragraphs[0])
+            print(3, response, "\n")
+
+            data_set_creation.save_data(response, file_path_gemma, save_path_gemma)
+
+    # model_mistral = "mistral"
+    # prompt_mistral = f'#based on the content, summarize the paragraph and return only a python list (nothing extra) which contains 2 different dictionaries of key value pairs covering key points of the paragraph. Example: {{"instruction": "natural language text input about a topic in this paragraph about AIDA", "response": "sentence explanation for the given prompt, per the instructions and context provided"}} ## content: "{paragraphs[0]}"'
+    # file_path_mistral = "../data/aida_dataset_mistral.json"
+    # save_path_mistral = "../data/aida_dataset_mistral.json"
+    # mistral = True
+    #
+    # model = "gemma2"
+    # prompt = f'#based on the ##content, summarize the paragraph and return only a python list (nothing extra) which contains 12 different dictionaries of key value pairs covering key points of the paragraph see ##Example for dictionary key value pair template. ##Example: {{"instruction": "natural language text input about a topic in this paragraph about AIDA", "response": "target output for the given prompt, per the instructions and context provided"}} ## content: "{paragraphs[0]}"'
+    # file_path = "../data/dataset_aida_gemma.json"
+    # save_path = "../data/dataset_aida_gemma.json"
+    # # gemma = False
+    #
+    #
+    # if mistral:
+    #     response = connect_model.request_model(model_mistral, prompt_mistral)
+    #     print(1, response)
+    #
+    #     ## TESTING ##
+    #     # confirm response format is correct with AI
+    #     # response = data_set_creation.confirm_data_format(model_mistral, response)
+    #     data_set_creation.confirm_data_format(model_mistral, response)
+    #     print(2, response)
+    #
+    #     # use AI to provide input to each dictionary
+    #     response = data_set_creation.create_input(model_mistral, response, paragraphs[0])
+    #     print(3, response)
+    #
+    #     data_set_creation.save_data(response, file_path_mistral, save_path_mistral)
+    #
+    #     ## OLD
+    #     # cleaned_dataset = data_set_creation.format_data(response)
+    #     # print(cleaned_dataset)
+    #
+    #     # data_set_creation.save_data(data_set_creation.format_data(response), file_path_mistral, save_path_mistral)
+    #
+    # else:
+    #     response = connect_model.request_model(model, prompt)
+    #     print(response)
+    #
+    #     cleaned_dataset = data_set_creation.format_data(response)
+    #     print(cleaned_dataset)
+    #
+    #     data_set_creation.save_data(data_set_creation.format_data(response), file_path, save_path)
 
 
 if __name__ == '__main__':
